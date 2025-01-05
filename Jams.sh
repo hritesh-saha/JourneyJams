62a23fa52ab90ee36ecb3b31221d425e7f31b332
Hritesh_Saha <hriteshsaha4@gmail.com>
2025-01-05 20:40:55 +0530

    minor change

--- Jams.sh
+++ Jams.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd JourneyJams || { echo "Failed to change directory"; exit 1; }
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+cd "$SCRIPT_DIR/JourneyJams" || { echo "Failed to change directory"; exit 1; }
+
 python downloader.py
